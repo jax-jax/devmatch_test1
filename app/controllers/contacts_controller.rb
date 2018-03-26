5c44bd5cddfd63744583c26df2280095c589481b
jax-jax <Watson@jax.local>
2018-03-26 13:53:50 -0700

    changed redirect paths

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,7 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
-  
+
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
@@ -14,19 +14,19 @@ class ContactsController < ApplicationController
         # An alert for Users HTML on the application.html so it's visible in any redirect-path.
         flash[:success] = "Message Sent."
         # Landing page after filling out the form
-        redirect_to contact_us_path
-        
+        redirect_to new_contact_path
+
     else
-      # Error Alert 
+      # Error Alert
         flash[:danger] = @contact.errors.full_messages.join(", ")
-        redirect_to contact_us_path
+        redirect_to new_contact_path
     end
-  end  
-  
+  end
+
   private
     # Security
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
-    end  
-  
+    end
+
 end
