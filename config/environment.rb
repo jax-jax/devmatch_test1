f1b41a7f3817dd9d619f44bba9b31bea5cb67c7e
jax-jax <Watson@jax.local>
2018-03-26 13:37:37 -0700

    added mailer settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'http://devmatch-dev.herokuapp.com',
+  :domain         => 'desolate-ridge-92436.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
