2991e5e2274343727b64fc72e2590d36c5a39a9f
jax-jax <Watson@jax.local>
2018-04-19 14:19:44 -0700

    Modified Users.js file

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,55 +3,6 @@
 and should send card info to Stripe. Stripe will return with a card token.*/
 
 //Document ready.
-$(document).on('turnolinks:load', function() {
-   var theForm = $('#pro-form');
-   var submitBtn = $('#form-submit-btn');
-   //Set Stripe public key.
-   Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content') );
-
-   //When user clicks form sumbit btn.
-   submitBtn.click(function(event){
-      //prevent default submission.
-      event.preventDefault();
-
-      //Collect the credit card fields.
-      var ccNum = $('#card_number').val(),
-          cvcNum = $('#card_code').val(),
-          expMonth = $('#card_month').val(),
-          expYear = $('#card_year').val();
-      //Send the card info to Stripe.
-      Stripe.createToken({
-         number: ccNum,
-         cvc: cvcNum,
-         exp_month: expmonth,
-         exp_year: expYear
-      }, stripeResponseHandler)
-
-   })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* global $, Stripe */
-//Document ready.
 $(document).on('turbolinks:load', function(){
   var theForm = $('#pro_form');
   var submitBtn = $('#form-submit-btn');
